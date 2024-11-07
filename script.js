be98bf7ce32405ffc73828275eb05a7d67c8a881
Lucas Young <layoung@email.wm.edu>
2024-11-07 12:54:09 -0500

    Finished tutorial.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
 
+function addTask(){
+    const taskInput = document.getElementById('taskInput').value;
+    
+    if(taskInput){
+        const newTask = document.createElement('li');
+        newTask.innerText= taskInput;
+
+        const taskList = document.getElementById('taskList')
+        taskList.appendChild(newTask)
+
+        const deleteButton = document.createElement("button");
+        deleteButton.addEventListener("click", ()=>taskList.removeChild(newTask))
+        deleteButton.className="button";
+        deleteButton.innerText="x";
+        newTask.appendChild(deleteButton)
+
+        document.getElementById('taskInput').value=''
+    }
+}
